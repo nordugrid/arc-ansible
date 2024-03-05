7226bc6c05f6e737ebf51914b44b29cdba2bb426
Sebastian Luna-Valero <sebastian-luna-valero@users.noreply.github.com>
2024-03-05 12:21:05 +0100

    Update runhello.sh

--- roles/arc_frontend/files/testing/runhello.sh
+++ roles/arc_frontend/files/testing/runhello.sh
@@ -3,4 +3,6 @@ echo "Computing md5sum for localfile";md5sum localfile>>output.txt;
 echo "Computing md5sum for remotefile";md5sum remotefile>>output.txt;
 echo "Am sleeping for 30 seconds"
 sleep 30
+echo "This job was executed on this node:"
+hostname -f
 exit 
